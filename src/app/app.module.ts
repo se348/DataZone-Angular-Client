22b5a60b0d66aeafee2b1321f0b487ac2d823106
yosephhabtamu <yosephhabtamu5@gmail.com>
2023-12-24 15:23:47 +0300

    DZ scaffold router outlet and creating tsconfig and package json reconf material package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxsModule } from '@ngxs/store';
 
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 import { JwtInterceptor } from './auth/services/jwt.interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ErrorInterceptor } from './core/interceptors/error.inteceptor';
-import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SharedModule } from './shared/shared.module';
@@ -20,11 +20,11 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     NgxsModule.forRoot([]),
-    NgxsLoggerPluginModule.forRoot(),
+    BrowserAnimationsModule, 
     NgxsStoragePluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule, 
     SharedModule
   ],
   providers: [
