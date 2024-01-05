b09c413ad439ff501564da96f3368aa647a07232
yosephhabtamu <yosephhabtamu5@gmail.com>
2024-01-05 21:04:10 +0300

    DZ fix same as above commit

--- src/app/shared/shared-component/shared-component.module.ts
+++ src/app/shared/shared-component/shared-component.module.ts
@@ -3,12 +3,13 @@ import { CommonModule, NgTemplateOutlet } from '@angular/common';
 import { CustomStepperComponent } from './custom-stepper/custom-stepper.component';
 import { CdkStepperModule } from '@angular/cdk/stepper';
 import { CustomToasterComponent } from './custom-toaster/custom-toaster.component';
+import { CustomSpinnerComponent } from './custom-progress-bar/spinner-dotted.component';
 
 
 
 @NgModule({
   declarations: [
-    CustomStepperComponent, CustomToasterComponent
+    CustomStepperComponent,CustomSpinnerComponent, CustomToasterComponent
   ],
   imports: [
     CommonModule, 
