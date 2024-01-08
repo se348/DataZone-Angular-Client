833d125c77dda359daf234a2d0025b4143e50b0f
Stargazing-11 <samuelkifle199314@gmail.com>
2024-01-08 22:24:49 +0300

    add store for company profile

--- src/app/core/constants/routes.ts
+++ src/app/core/constants/routes.ts
@@ -5,10 +5,9 @@ export const REGISTER_ROUTE = 'register';
 export const LANDING_PAGE_ROUTE = 'landing-page';
 export const COMPLETE_USER_PROFILE_PAGE_ROUTE = 'complete-user-profile-page';
 export const COMPLETE_PROFILE_PAGE_ROUTE = 'complete-profile';
+export const EDIT_COMPANY_PROFILE_PAGE_ROUTE = 'edit-company-profile';
 
 export const CONFIRM_EMAIL_ROUTE = 'api/v1/account/confirmEmail';
 export const ROLES_LIST_ROUTE = 'roles-list'
 export const EMAIL_SENT_ROUTE =  'email-sent';
-
-
 
