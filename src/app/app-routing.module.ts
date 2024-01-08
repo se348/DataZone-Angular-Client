07bb083d5b62d5e596d191bba5ab9373b09ff970
Stargazing-11 <samuelkifle199314@gmail.com>
2024-01-08 22:26:43 +0300

    add routing for edit profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {
   COMPLETE_USER_PROFILE_PAGE_ROUTE,
   LANDING_PAGE_ROUTE,
+  EDIT_COMPANY_PROFILE_PAGE_ROUTE
 } from './core/constants/routes';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { CompleteUserProfileComponent } from './user-profile/components/complete-user-profile/complete-user-profile.component';
+import { CompleteUserProfileComponent, } from './user-profile/components/complete-user-profile/complete-user-profile.component';
+import { EditCompanyProfileComponent } from './company-profile/components/edit-company-profile/edit-company-profile.component';
 
 const routes: Routes = [
   {
@@ -19,6 +21,10 @@ const routes: Routes = [
   {
     path: COMPLETE_USER_PROFILE_PAGE_ROUTE,
     component: CompleteUserProfileComponent,
+  },
+  {
+    path: EDIT_COMPANY_PROFILE_PAGE_ROUTE,
+    component: EditCompanyProfileComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,8 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { ToastState } from './core/store/toast.state';
-import { CompleteUserProfileComponent } from './user-profile/components/complete-user-profile/complete-user-profile.component';
 import { UserProfileModule } from './user-profile/user-profile.module';
+import { CompanyProfileModule } from './company-profile/company-profile.module';
 @NgModule({
   declarations: [AppComponent, LandingPageComponent],
   providers: [
@@ -44,6 +44,7 @@ import { UserProfileModule } from './user-profile/user-profile.module';
     AuthModule,
     AppRoutingModule,
     UserProfileModule,
+    CompanyProfileModule
   ],
 })
 export class AppModule {}
