96535ddeba1134f4d408e73a7af9142d117de339
destifo <estifanosbireda@gmail.com>
2024-01-08 20:54:29 +0300

    DZ-83 feat: add last parts

--- src/app/auth/components/roles-list/roles-list.component.ts
+++ src/app/auth/components/roles-list/roles-list.component.ts
@@ -5,38 +5,37 @@ import { Observable } from 'rxjs';
 import { RxState } from '@rx-angular/state';
 import { RoleFacade } from '../../facades/role.facades';
 
-
-
 interface RolesListComponentState {
   roles: Role[];
 }
 
 const initRolesListComponentState: RolesListComponentState = {
-  roles: []
-}
-
+  roles: [],
+};
 
 @Component({
   selector: 'app-roles-list',
   standalone: false,
   templateUrl: './roles-list.component.html',
-  styleUrl: './roles-list.component.scss'
+  styleUrl: './roles-list.component.scss',
 })
 export class RolesListComponent implements OnInit {
   roles$: Observable<Role[]> = this.state.select('roles');
   roles: Role[] = [];
+  displayedColumns = ['name', 'createdAt'];
 
-  constructor(private state: RxState<RolesListComponentState>, private roleFacade: RoleFacade) {
+  constructor(
+    private state: RxState<RolesListComponentState>,
+    private roleFacade: RoleFacade
+  ) {
     state.set(initRolesListComponentState);
     state.connect('roles', roleFacade.roles$);
   }
 
   ngOnInit(): void {
-    this.roles$.subscribe(
-      (roles) => {
-        this.roles = roles;
-      }
-    );
+    this.roleFacade.dispatchGetRoles();
+    this.roles$.subscribe((roles) => {
+      this.roles = roles;
+    });
   }
-
 }
