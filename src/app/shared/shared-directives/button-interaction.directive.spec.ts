0fe521d9a09730f10788bbfcc3eb1776c7532632
yosephhabtamu <yosephhabtamu5@gmail.com>
2024-01-12 14:33:25 +0300

    removed ripple effect directive cuz it aint working

--- src/app/shared/shared-directives/button-interaction.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { ButtonInteractionDirective } from './button-interaction.directive';
-
-// describe('ButtonInteractionDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new ButtonInteractionDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
--- src/app/shared/shared-directives/shared-directives.module.ts
+++ src/app/shared/shared-directives/shared-directives.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonInteractionDirective } from './button-interaction.directive';
-
 
 
 @NgModule({
-  declarations: [
-    ButtonInteractionDirective
-  ],
+  declarations: [],
   imports: [
     CommonModule
   ]
