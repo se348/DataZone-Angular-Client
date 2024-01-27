40db1db9284789ecd3a1ccd95b0ef9e1bc0800a7
Stargazing-11 <samuelkifle199314@gmail.com>
2024-01-27 15:28:20 +0300

    add succes component

---
+++ src/app/shared/shared-component/success-page/success-page.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { StateMessageService } from 'src/app/core/service/state-message.service';
+
+@Component({
+  selector: 'app-success-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './success-page.component.html',
+  styleUrl: './success-page.component.scss',
+})
+export class SuccessPageComponent {
+  message: string | undefined;
+  constructor(
+    private router: Router,
+    private stateMessageService: StateMessageService
+  ) {}
+
+  ngOnInit() {
+    this.stateMessageService.getStateMessage().subscribe((message) => {
+      this.message = message!;
+    });
+  }
+
+  goBack() {
+    this.router.navigateByUrl('/forget-password');
+  }
+}
