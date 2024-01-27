976b6ae5fdc5fd187c47806ec06be988579b1900
Stargazing-11 <samuelkifle199314@gmail.com>
2024-01-27 16:19:59 +0300

    add success and error indicator components

--- src/app/shared/shared-component/shared-component.module.ts
+++ src/app/shared/shared-component/shared-component.module.ts
@@ -9,17 +9,21 @@ import { SharedModule } from '../shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
 import { MatIconModule } from '@angular/material/icon';
+import { SuccessIndicatorComponent } from "./success-indicator/success-indicator.component";
+import { ErrorIndicatorComponent } from "./error-indicator/error-indicator.component";
 
 
 
 @NgModule({
-  declarations: [
-    CustomStepperComponent, DragDropComponent, ProfilePictureComponent,CustomSpinnerComponent, CustomToasterComponent
-  ],
-  imports: [
-    CommonModule, 
-    CdkStepperModule, ReactiveFormsModule, MatIconModule
-  ],
-  exports:[CustomStepperComponent, CustomToasterComponent, DragDropComponent, ProfilePictureComponent]
+    declarations: [
+        CustomStepperComponent, DragDropComponent, ProfilePictureComponent, CustomSpinnerComponent, CustomToasterComponent
+    ],
+    exports: [CustomStepperComponent, CustomToasterComponent, DragDropComponent, ProfilePictureComponent],
+    imports: [
+        CommonModule,
+        CdkStepperModule, ReactiveFormsModule, MatIconModule,
+        SuccessIndicatorComponent,
+        ErrorIndicatorComponent
+    ]
 })
 export class SharedComponentModule { }
