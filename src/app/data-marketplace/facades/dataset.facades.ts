012e98462167790f22b49d7730f5e3b7ad4d5949
Semir Ahmed <semir2578@gmail.com>
2024-01-28 22:09:45 +0300

    create dataset list functionality

---
+++ src/app/data-marketplace/facades/dataset.facades.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DatasetFacade {
+
+  constructor(private store: Store) {}
+
+  
+}
---
+++ src/app/data-marketplace/services/dataset.service.ts
@@ -0,0 +1,16 @@
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import { HttpClient } from '@angular/common/http';
+// import { UPLOAD_DATASET_URL } from 'src/app/core/constants/api-endpoints';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class DatasetService {
+
+//   constructor(private http: HttpClient) {}
+
+//   uploadDataset(request: FormData): Observable<_> {
+//     return this.http.post<_>(UPLOAD_DATASET_URL, request);
+//   }
+// }
