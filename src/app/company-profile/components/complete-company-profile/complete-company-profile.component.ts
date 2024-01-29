62b54a47cfdb20e679d11e217aec2b9b2fe9fe89
yosephhabtamu <yosephhabtamu5@gmail.com>
2024-01-29 12:37:06 +0300

    DZ feat user profile impl

---
+++ src/app/company-profile/components/complete-company-profile/complete-company-profile.component.ts
@@ -0,0 +1,179 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SharedComponentModule } from '../../../shared/shared-component/shared-component.module';
+import {
+  CdkStep,
+  CdkStepper,
+  CdkStepperModule,
+  StepperOrientation,
+  StepperSelectionEvent,
+} from '@angular/cdk/stepper';
+import { CustomStepperComponent } from 'src/app/shared/shared-component/custom-stepper/custom-stepper.component';
+import { RxState } from '@rx-angular/state';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+  AbstractControl,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { LANDING_PAGE_ROUTE } from 'src/app/core/constants/routes';
+import { AuthFacade } from '../../../auth/facades/auth.facades';
+import {CompanyProfileFacade} from "../../facades/company-profile.facades";
+import {CompanyProfile, CompanyProfileResponse, IndustryTypes} from "../../models/company-profile.model";
+
+interface LoginComponentState {
+  isAuthenticated: boolean;
+  companyProfile: CompanyProfile | null;
+}
+
+const initLoginComponentState: LoginComponentState = {
+  isAuthenticated: false,
+  companyProfile: null,
+};
+
+@Component({
+  selector: 'app-complete-company-profile',
+  templateUrl: './complete-company-profile.component.html',
+  styleUrl: './complete-company-profile.component.scss',
+  providers: [RxState, { provide: CdkStepper }],
+})
+export class CompleteCompanyProfileComponent {
+  isAuthenticated$: Observable<boolean> = this.state.select('isAuthenticated');
+  profileControl!: FormGroup;
+  documentControl!: FormGroup;
+  paymentControl!: FormGroup;
+  profilePic?: File;
+  businessLicense?: File;
+
+  companyProfile: CompanyProfile | null = null;
+
+  companyProfile$ = this.state.select('companyProfile');
+
+  @Input() submitclicked!: EventEmitter<boolean>;
+
+  totalIndustrytypes = Object.keys(IndustryTypes).length;
+  get getIndustryTypes(): Array<string> {
+    return Object.keys(IndustryTypes);
+  }
+
+  currentStepIndex = 0;
+  imageSize = 100;
+
+  constructor(
+    private readonly authFacade: AuthFacade,
+    private readonly companyProfileFacade: CompanyProfileFacade,
+    private readonly state: RxState<LoginComponentState>,
+    private readonly router: Router,
+    private readonly formBuilder: FormBuilder
+  ) {
+    this.state.set(initLoginComponentState);
+    this.state.connect('isAuthenticated', this.authFacade.isAuthenticated$);
+    this.state.connect('companyProfile', this.companyProfileFacade.companyProfile$);
+
+    this.profileControl = this.formBuilder.group({
+      companyName: ['', [Validators.required]],
+      companyEmail: ['', [Validators.required, Validators.email]],
+      companyWebSite: [''],
+    });
+    this.documentControl = this.formBuilder.group({
+      companyAddress: [''],
+      industryType: ['', [Validators.required]],
+      businessLicense: ['', [Validators.required]],
+      profilePicture: [''],
+    });
+    this.paymentControl = this.formBuilder.group({});
+  }
+
+  ngOnInit(): void {
+    this.companyProfile$.subscribe((profile) => {
+      this.companyProfile = profile;
+    });
+    this.isAuthenticated$.subscribe((isAuth) => {
+      if (isAuth && this.companyProfile !== null)
+        this.router.navigate([LANDING_PAGE_ROUTE]);
+    });
+    this.currentStepIndexControl.valueChanges.subscribe((value) => {
+      this.currentStepIndex = value;
+    });
+  }
+  currentStepIndexControl = new FormControl();
+
+  stepperTitle = 'Complete Company Profile';
+
+  handleSubmitEvent(event: any) {
+    if (event) this.saveForm();
+  }
+
+  saveForm() {
+    const { valid, touched, dirty } = this.profileControl!;
+    if (valid && (touched || dirty)) {
+      const {
+        companyName,
+        companyEmail,
+        companyWebSite,
+        companyAddress,
+        industryType,
+      } = this.profileControl!.value;
+
+      const formData = this.organizeFormData(
+        companyName,
+        companyEmail,
+        companyWebSite,
+        companyAddress,
+        industryType
+      );
+      this.authFacade.dispatchCompleteCompanyProfile(formData);
+      this.router.navigate([LANDING_PAGE_ROUTE]);
+    }
+  }
+
+  organizeFormData(
+    companyName: string,
+    companyEmail: string,
+    industryType: IndustryTypes,
+    companyWebSite?: string,
+    companyAddress?: string
+  ): FormData {
+    const formData = new FormData();
+    formData.append('CompanyName', companyName);
+    formData.append('CompanyEmail', companyEmail);
+    if (companyWebSite) formData.append('CompanyWebSite', companyWebSite);
+    if (companyAddress) formData.append('CompanyAddress', companyAddress);
+    formData.append('IndustryType', industryType);
+    if (this.profilePic) formData.append('ProfilePic', this.profilePic);
+
+    return formData;
+  }
+
+  getProfilePic(file: File) {
+    this.profilePic = file;
+  }
+
+  getUploadedLicense(file?: File) {
+    this.documentControl.controls['businessLicense'].setValue(file);
+    if (file) this.businessLicense = file;
+    else return;
+  }
+
+  getInitials() {
+    const names = this.profileControl.controls['companyName'].value
+      .trim()
+      .split(' ');
+    let initials = '';
+    for (let i = 0; i < names.length; i++) {
+      if (names[i] !== '') {
+        initials += names[i][0];
+        if (initials.length === 2) {
+          break;
+        }
+      }
+    }
+    return initials.toUpperCase();
+  }
+
+  handleSelectionChange(index: number) {
+    this.currentStepIndexControl.setValue(index);
+  }
+}
---
+++ src/app/company-profile/facades/company-profile.facades.ts
@@ -0,0 +1,26 @@
+import { CompanyProfileSelector } from '../store/company-profile.selector';
+import { Observable } from 'rxjs';
+import {CompanyProfile, CompanyProfileResponse} from '../models/company-profile.model';
+import { Injectable } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import {
+  EditCompanyProfile,
+  GetCompanyProfile,
+} from '../store/company-profile.actions';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CompanyProfileFacade {
+  @Select(CompanyProfileSelector.companyProfile)
+  companyProfile$!: Observable<CompanyProfile>;
+  constructor(private store: Store) {}
+
+  dispatchGetCompanyProfile(request: string) {
+    this.store.dispatch(new GetCompanyProfile(request));
+  }
+
+  dispatchEditCompanyProfile(request: Partial<CompanyProfile>) {
+    this.store.dispatch(new EditCompanyProfile(request));
+  }
+}
