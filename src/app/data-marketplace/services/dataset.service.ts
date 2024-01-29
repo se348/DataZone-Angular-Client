31ecf085b52b13f7b4296e47bb9178fabbe61d8f
Semir Ahmed <semir2578@gmail.com>
2024-01-29 09:10:23 +0300

    fix some issues dataset

--- src/app/data-marketplace/services/dataset.service.ts
+++ src/app/data-marketplace/services/dataset.service.ts
@@ -1,16 +1,14 @@
-// import { Injectable } from '@angular/core';
-// import { Observable } from 'rxjs';
-// import { HttpClient } from '@angular/common/http';
-// import { UPLOAD_DATASET_URL } from 'src/app/core/constants/api-endpoints';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { UPLOAD_DATASET_URL } from 'src/app/core/constants/api-endpoints';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class DatasetService {
+@Injectable({
+  providedIn: 'root'
+})
+export class DatasetService {
 
-//   constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-//   uploadDataset(request: FormData): Observable<_> {
-//     return this.http.post<_>(UPLOAD_DATASET_URL, request);
-//   }
-// }
+  
+}
