adc34e738902c52279cfa3c5c2360c58f91766af
Stargazing-11 <samuelkifle199314@gmail.com>
2024-01-29 10:17:53 +0300

    home component

--- src/app/main-landing-page/components/home/home.component.ts
+++ src/app/main-landing-page/components/home/home.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LOGIN_ROUTE } from 'src/app/core/constants/routes';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  constructor(private readonly router: Router,) {}
+
+  navigateToLogin() {
+    this.router.navigate([LOGIN_ROUTE]);
+  }
+}
