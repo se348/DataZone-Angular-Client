acaf043e17fa846722d375d4166afd4bcdeaacf8
destifo <estifanosbireda@gmail.com>
2024-03-23 20:16:22 +0300

    feat: add dataset reports list page

--- src/app/data-marketplace/dataset-reports/models/dataset-report.model.ts
+++ src/app/data-marketplace/dataset-reports/models/dataset-report.model.ts
@@ -1,5 +1,20 @@
+export enum InfringementType {
+  CopyRight,
+  FalseData,
+  Privacy,
+  Other
+}
 
+export enum DatasetReportStatus {
+  Approved,
+  Rejected,
+  Pending,
+}
 
 export interface DatasetReport {
-    
+  id: string;
+  message: string;
+  datasetId: string;
+  status: DatasetReportStatus;
+  infringementType: InfringementType;
 }
