0f4c4ee69b8c8fd6d68e05952383a6722b49e08f
Stargazing <samuelkifle199314@@gmail.com>
2024-03-24 01:35:25 +0300

    update dataset faced

--- src/app/data-marketplace/components/dataset-preview/dataset-preview.component.ts
+++ src/app/data-marketplace/components/dataset-preview/dataset-preview.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatasetFacade } from '../../facades/dataset.facades';
+import { RxState } from '@rx-angular/state';
+
+interface DatasetPreviewComponentState {
+  dataset: any;
+}
+
+const initDatasetPreviewComponentState: DatasetPreviewComponentState = {
+  dataset: null,
+};
 
 @Component({
   selector: 'app-dataset-preview',
   templateUrl: './dataset-preview.component.html',
-  styleUrl: './dataset-preview.component.scss'
+  styleUrl: './dataset-preview.component.scss',
+  providers: [RxState],
 })
-export class DatasetPreviewComponent {
+export class DatasetPreviewComponent implements OnInit {
+  dataSet$ = this.state.select('dataset');
+  constructor(
+    private readonly datasetFacade: DatasetFacade,
+    private readonly state: RxState<DatasetPreviewComponentState>
+  ) {
+    this.state.set(initDatasetPreviewComponentState);
+    this.state.connect('dataset', this.datasetFacade.dataset$);
+  }
 
+  ngOnInit(): void {
+    this.dataSet$.subscribe((dataset) => {
+      if (dataset) {
+        this.getDataset(dataset.id);
+      }
+    });
+  }
+
+  getDataset(id: string) {
+    this.datasetFacade.dispatchGetDataset(id);
+  }
 }
--- src/app/data-marketplace/facades/dataset.facades.ts
+++ src/app/data-marketplace/facades/dataset.facades.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Store } from '@ngxs/store';
-import { DatasetUploadRequest } from '../models/dataset.model';
-import { DatasetUpload, GetDatasetList } from '../store/dataset.action';
+import { Select, Store } from '@ngxs/store';
+import {
+  DatasetListModel,
+  DatasetUploadRequest,
+} from '../models/dataset.model';
+import {
+  DatasetUpload,
+  GetDataset,
+  GetDatasetList,
+} from '../store/dataset.action';
+import { DatasetSelector } from '../store/dataset.selector';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DatasetFacade {
+  dataSet$ = this.store.select(DatasetSelector.dataset);
+  dataSetList$ = this.store.select(DatasetSelector.datasetList);
+
+  @Select(DatasetSelector.dataset)
+  dataset$!: Observable<DatasetListModel>;
 
   constructor(private store: Store) {}
 
@@ -14,7 +28,11 @@ export class DatasetFacade {
     this.store.dispatch(new DatasetUpload(request));
   }
 
-  dispatchGetDatasets(){
+  dispatchGetDatasets() {
     this.store.dispatch(new GetDatasetList());
+  }
+
+  dispatchGetDataset(id: string) {
+    this.store.dispatch(new GetDataset(id));
   }
 }
